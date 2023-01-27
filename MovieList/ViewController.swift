7a048c69719b210e366c667b19033a70a7a1829a
wyn <k022298@gmail.com>
2023-01-27 16:35:33 +0000

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by wyn on 2023/1/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
