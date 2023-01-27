b0e32dbd5d5397bf7bc9adbdf8ec63daaad67834
wyn <k022298@gmail.com>
2023-01-27 16:48:23 +0000

    Inin project
    remove Main.storyboard, ViewController
    change iOS target version to 12.2
    remove SceneDelegate

--- MovieList/LoginViewController.swift
+++ MovieList/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  MovieList
 //
 //  Created by wyn on 2023/1/27.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
