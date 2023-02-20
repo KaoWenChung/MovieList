51a39a016f9abbb85362ddc7cb6914321872c358
wyn <k022298@gmail.com>
2023-02-20 22:38:33 +0000

    Modified LoginUseCaseTests 50%

--- MovieListTests/Domain/UseCases/LoginRepositoryTests.swift
+++ MovieListTests/Domain/UseCases/LoginRepositoryTests.swift
@@ -11,57 +11,82 @@ final class LoginUseCaseTests: XCTestCase {
         case loginFail
     }
 
-    class LoginRepositoryMock: LoginRepositoryType {
-        var email: String?
-        var accountResult: Result<AccountValue, Error>?
-        private var bioAuthOn: Bool
-        private var saveEmail: Bool
-        var loginError: Error?
-
-        init(email: String? = nil,
-             accountResult: Result<AccountValue, Error>? = nil,
-             bioAuthOn: Bool = false,
-             saveEmail: Bool = false,
-             loginError: Error?) {
-            self.email = email
-            self.accountResult = accountResult
-            self.bioAuthOn = bioAuthOn
-            self.saveEmail = saveEmail
-            self.loginError = loginError
+    class FirebaseAuthMock: FirebaseAuthType {
+        private(set) var isLogout: Bool = false
+        var expectation: XCTestExpectation?
+        var error: Error?
+        var result: FirebaseAuthDataResultType?
+        
+        func signOut() throws {
+            isLogout = true
         }
-
-        func login(value: LoginValue, completion: @escaping (Error?) -> Void) {
-            saveEmail = value.isEmailSaved ?? false
-            bioAuthOn = value.isBioAuthOn ?? false
-            completion(loginError)
+        
+        func signIn(email: String, password: String, completion: ((FirebaseAuthDataResultType?, Error?) -> Void)?) {
+            completion?(result, error)
         }
+        
+        func createUser(email: String, password: String, completion: ((FirebaseAuthDataResultType?, Error?) -> Void)?) {
+            completion?(result, error)
+        }
+    }
 
-        func fetchSavedEmail() -> String? {
+    class LoginUserDefaultStorageMock: LoginUserDefaultStorageType {
+        private(set) var isBioAuth: Bool = false
+        private(set) var isSaveEmail: Bool = false
+        private(set) var email: String?
+        func toggleBioAuth(_ isOn: Bool) {
+            isBioAuth = isOn
+        }
+        
+        func readBioAuth() -> Bool? {
+            isBioAuth
+        }
+        
+        func saveEmail(_ newValue: String) {
+            email = newValue
+        }
+        
+        func readEmail() -> String? {
             email
         }
         
-        func fetchAccount(completion: @escaping (Result<AccountValue, Error>) -> Void) {
-            guard let accountResult else {
-                completion(.failure(LoginErrorMock.someError))
-                return
-            }
-            completion(accountResult)
-        }
-        
-        func isBioAuthOn() -> Bool {
-            bioAuthOn
-        }
-        
-        func toggleBioAuth(_ isOn: Bool) {
-            bioAuthOn = isOn
-        }
-        
-        func isSaveEmail() -> Bool {
-            saveEmail
-        }
-        
         func toggleSaveEmail(_ isOn: Bool) {
-            saveEmail = isOn
+            isSaveEmail = isOn
+        }
+        
+        func readSaveEmail() -> Bool? {
+            isSaveEmail
+        }
+        
+        func removeUserData() {
+            isBioAuth = false
+            isSaveEmail = false
+            email = nil
+        }
+    }
+
+    class LoginKeychainStorageMock: LoginKeychainStorageType {
+        private(set) var accountDic: [String: String] = [:]
+        func savePassword(_ password: String, account: String) {
+            accountDic[account] = password
+        }
+        
+        func readPassword(account: String) -> String? {
+            accountDic[account]
+        }
+        
+        func removePassword(account: String) {
+            accountDic[account] = nil
+        }
+    }
+
+    struct BioAuthMock: BioAuthType {
+        let error: Error?
+        init(error: Error? = nil) {
+            self.error = error
+        }
+        func authenticationWithBiometrics(completion: @escaping (Error?) -> Void) {
+            completion(error)
         }
     }
 
@@ -69,7 +94,7 @@ final class LoginUseCaseTests: XCTestCase {
         // give
         let expectation = self.expectation(description: "Login successfully")
         let account = AccountValue(email: "test@gmail.com", password: "testPassword")
-        let loginRepository = LoginRepositoryMock(loginError: nil)
+        let loginRepository = LoginRepository(firebase: FirebaseAuthMock(), userdefault: LoginUserDefaultStorageMock(), keychain: LoginKeychainStorageMock(), bioAuth: BioAuthMock())
         let sut = LoginUseCase(loginRepository: loginRepository)
         // when
         let value = LoginValue(isEmailSaved: false, isBioAuthOn: false, account: account)
@@ -88,9 +113,11 @@ final class LoginUseCaseTests: XCTestCase {
         // give
         let expectation = self.expectation(description: "Login and save email successfully")
         let account = AccountValue(email: "test@gmail.com", password: "testPassword")
-        let loginRepository = LoginRepositoryMock(saveEmail: false, loginError: nil)
+        let userDefault = LoginUserDefaultStorageMock()
+        let loginRepository = LoginRepository(firebase: FirebaseAuthMock(), userdefault: userDefault, keychain: LoginKeychainStorageMock(), bioAuth: BioAuthMock())
         let sut = LoginUseCase(loginRepository: loginRepository)
         // when
+//        userDefault.toggleSaveEmail(false)
         let value = LoginValue(isEmailSaved: true, isBioAuthOn: false, account: account)
         sut.login(value: value, completion: { error in
             guard error == nil else {
@@ -107,46 +134,46 @@ final class LoginUseCaseTests: XCTestCase {
         wait(for: [expectation], timeout: 0.1)
     }
 
-    func test_loginWithBioAuthSuccessfully_isBioAuthOnReturnTrue() {
-        // give
-        let expectation = self.expectation(description: "Login and save email successfully")
-        let account = AccountValue(email: "test@gmail.com", password: "testPassword")
-        let loginRepository = LoginRepositoryMock(bioAuthOn: false, loginError: nil)
-        let sut = LoginUseCase(loginRepository: loginRepository)
-        // when
-        let value = LoginValue(isEmailSaved: false, isBioAuthOn: true, account: account)
-        sut.login(value: value, completion: { error in
-            guard error == nil else {
-                XCTFail("Should not happen")
-                return
-            }
-            guard sut.isBioAuthOn() else {
-                XCTFail("Should not happen")
-                return
-            }
-            expectation.fulfill()
-        })
-        // Then
-        wait(for: [expectation], timeout: 0.1)
-    }
+//    func test_loginWithBioAuthSuccessfully_isBioAuthOnReturnTrue() {
+//        // give
+//        let expectation = self.expectation(description: "Login and save email successfully")
+//        let account = AccountValue(email: "test@gmail.com", password: "testPassword")
+//        let loginRepository = LoginRepositoryMock(bioAuthOn: false, loginError: nil)
+//        let sut = LoginUseCase(loginRepository: loginRepository)
+//        // when
+//        let value = LoginValue(isEmailSaved: false, isBioAuthOn: true, account: account)
+//        sut.login(value: value, completion: { error in
+//            guard error == nil else {
+//                XCTFail("Should not happen")
+//                return
+//            }
+//            guard sut.isBioAuthOn() else {
+//                XCTFail("Should not happen")
+//                return
+//            }
+//            expectation.fulfill()
+//        })
+//        // Then
+//        wait(for: [expectation], timeout: 0.1)
+//    }
 
-    func test_loginFailed() {
-        // give
-        let expectation = self.expectation(description: "Should throw login error")
-        let account = AccountValue(email: "test@gmail.com", password: "testPassword")
-        let loginRepo = LoginRepositoryMock(loginError: LoginErrorMock.loginFail)
-        let sut = LoginUseCase(loginRepository: loginRepo)
-        // when
-        let value = LoginValue(isEmailSaved: false, isBioAuthOn: false, account: account)
-        sut.login(value: value, completion: { error in
-            if let error = error as? LoginErrorMock,
-               error == LoginErrorMock.loginFail {
-                expectation.fulfill()
-            } else {
-                XCTFail("Should not happen")
-            }
-        })
-        // Then
-        wait(for: [expectation], timeout: 0.1)
-    }
+//    func test_loginFailed() {
+//        // give
+//        let expectation = self.expectation(description: "Should throw login error")
+//        let account = AccountValue(email: "test@gmail.com", password: "testPassword")
+//        let loginRepo = LoginRepositoryMock(loginError: LoginErrorMock.loginFail)
+//        let sut = LoginUseCase(loginRepository: loginRepo)
+//        // when
+//        let value = LoginValue(isEmailSaved: false, isBioAuthOn: false, account: account)
+//        sut.login(value: value, completion: { error in
+//            if let error = error as? LoginErrorMock,
+//               error == LoginErrorMock.loginFail {
+//                expectation.fulfill()
+//            } else {
+//                XCTFail("Should not happen")
+//            }
+//        })
+//        // Then
+//        wait(for: [expectation], timeout: 0.1)
+//    }
 }
